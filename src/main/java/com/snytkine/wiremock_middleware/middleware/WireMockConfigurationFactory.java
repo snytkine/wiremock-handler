f1a317c9e0ee3bd36c01730d1bbd25054912f7fc
Dmitri Snytkine <d.snytkine@gmail.com>
2025-12-02 05:25:42 -0500

    Using wiremock version 3.10.0 as latest stable. From 3.11 there is an issue with missing httpclient5 missing. Maybe it works with Java 21 but not without extra dependency in java 17

--- src/main/java/com/snytkine/wiremock_middleware/middleware/WireMockConfigurationFactory.java
+++ src/main/java/com/snytkine/wiremock_middleware/middleware/WireMockConfigurationFactory.java
@@ -9,49 +9,49 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WireMockConfigurationFactory {
 
-        private final WireMockProperties wireMockProperties;
+    private final WireMockProperties wireMockProperties;
 
-        public WireMockConfigurationFactory(WireMockProperties wireMockProperties) {
-                this.wireMockProperties = wireMockProperties;
+    public WireMockConfigurationFactory(WireMockProperties wireMockProperties) {
+        this.wireMockProperties = wireMockProperties;
+    }
+
+    @Bean
+    public WireMockConfiguration wireMockConfiguration() {
+        WireMockConfiguration wireMockConfiguration = new WireMockConfiguration();
+
+        Optional.ofNullable(wireMockProperties.getContainerThreads())
+                .ifPresent(wireMockConfiguration::containerThreads);
+        Optional.ofNullable(wireMockProperties.getAsynchronousResponseEnabled())
+                .ifPresent(wireMockConfiguration::asynchronousResponseEnabled);
+        Optional.ofNullable(wireMockProperties.getAsynchronousResponseThreads())
+                .ifPresent(wireMockConfiguration::asynchronousResponseThreads);
+        Optional.ofNullable(wireMockProperties.getRootDirectory())
+                .ifPresent(wireMockConfiguration::usingFilesUnderDirectory);
+
+        if (Boolean.TRUE.equals(wireMockProperties.getJournalDisabled())) {
+            wireMockConfiguration.disableRequestJournal();
         }
 
-        @Bean
-        public WireMockConfiguration wireMockConfiguration() {
-                WireMockConfiguration wireMockConfiguration = new WireMockConfiguration();
+        Optional.ofNullable(wireMockProperties.getMaxRequestJournalEntries())
+                .ifPresent(wireMockConfiguration::maxRequestJournalEntries);
+        Optional.ofNullable(wireMockProperties.getGzipDisabled())
+                .ifPresent(wireMockConfiguration::gzipDisabled);
+        Optional.ofNullable(wireMockProperties.getDisableOptimizeXmlFactories())
+                .ifPresent(wireMockConfiguration::disableOptimizeXmlFactoriesLoading);
+        Optional.ofNullable(wireMockProperties.getStubCorsEnabled())
+                .ifPresent(wireMockConfiguration::stubCorsEnabled);
+        Optional.ofNullable(wireMockProperties.getStubRequestLoggingDisabled())
+                .ifPresent(wireMockConfiguration::stubRequestLoggingDisabled);
+        Optional.ofNullable(wireMockProperties.isTemplatingEnabled())
+                .ifPresent(wireMockConfiguration::templatingEnabled);
+        Optional.ofNullable(wireMockProperties.getMappingsClassPath())
+                .ifPresent(wireMockConfiguration::usingFilesUnderClasspath);
+        Optional.ofNullable(wireMockProperties.isProxyPassThrough())
+                .ifPresent(wireMockConfiguration::proxyPassThrough);
 
-                Optional.ofNullable(wireMockProperties.getContainerThreads())
-                                .ifPresent(wireMockConfiguration::containerThreads);
-                Optional.ofNullable(wireMockProperties.getAsynchronousResponseEnabled())
-                                .ifPresent(wireMockConfiguration::asynchronousResponseEnabled);
-                Optional.ofNullable(wireMockProperties.getAsynchronousResponseThreads())
-                                .ifPresent(wireMockConfiguration::asynchronousResponseThreads);
-                Optional.ofNullable(wireMockProperties.getRootDirectory())
-                                .ifPresent(wireMockConfiguration::usingFilesUnderDirectory);
+        wireMockConfiguration.trustAllProxyTargets(true);
+        wireMockConfiguration.requestJournalDisabled();
 
-                if (Boolean.TRUE.equals(wireMockProperties.getJournalDisabled())) {
-                        wireMockConfiguration.disableRequestJournal();
-                }
-
-                Optional.ofNullable(wireMockProperties.getMaxRequestJournalEntries())
-                                .ifPresent(wireMockConfiguration::maxRequestJournalEntries);
-                Optional.ofNullable(wireMockProperties.getGzipDisabled())
-                                .ifPresent(wireMockConfiguration::gzipDisabled);
-                Optional.ofNullable(wireMockProperties.getDisableOptimizeXmlFactories())
-                                .ifPresent(wireMockConfiguration::disableOptimizeXmlFactoriesLoading);
-                Optional.ofNullable(wireMockProperties.getStubCorsEnabled())
-                                .ifPresent(wireMockConfiguration::stubCorsEnabled);
-                Optional.ofNullable(wireMockProperties.getStubRequestLoggingDisabled())
-                                .ifPresent(wireMockConfiguration::stubRequestLoggingDisabled);
-                Optional.ofNullable(wireMockProperties.isTemplatingEnabled())
-                                .ifPresent(wireMockConfiguration::templatingEnabled);
-                Optional.ofNullable(wireMockProperties.getMappingsClassPath())
-                                .ifPresent(wireMockConfiguration::usingFilesUnderClasspath);
-                Optional.ofNullable(wireMockProperties.isProxyPassThrough())
-                                .ifPresent(wireMockConfiguration::proxyPassThrough);
-
-                wireMockConfiguration.trustAllProxyTargets(true);
-                wireMockConfiguration.requestJournalDisabled();
-
-                return wireMockConfiguration;
-        }
+        return wireMockConfiguration;
+    }
 }
