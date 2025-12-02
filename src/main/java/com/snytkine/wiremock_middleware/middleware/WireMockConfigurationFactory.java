7327f051a46df87ad16f15da03d3b01db8197720
Dmitri Snytkine <d.snytkine@gmail.com>
2025-12-01 19:20:44 -0500

    Updated config factory to enable templating. Now templating works in response1.json under __files

--- src/main/java/com/snytkine/wiremock_middleware/middleware/WireMockConfigurationFactory.java
+++ src/main/java/com/snytkine/wiremock_middleware/middleware/WireMockConfigurationFactory.java
@@ -9,51 +9,52 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WireMockConfigurationFactory {
 
-    private final WireMockProperties wireMockProperties;
+        private final WireMockProperties wireMockProperties;
 
-    public WireMockConfigurationFactory(WireMockProperties wireMockProperties) {
-        this.wireMockProperties = wireMockProperties;
-    }
-
-    @Bean
-    public WireMockConfiguration wireMockConfiguration() {
-        WireMockConfiguration wireMockConfiguration = new WireMockConfiguration();
-
-        Optional.ofNullable(wireMockProperties.getContainerThreads())
-                .ifPresent(wireMockConfiguration::containerThreads);
-        Optional.ofNullable(wireMockProperties.getAsynchronousResponseEnabled())
-                .ifPresent(wireMockConfiguration::asynchronousResponseEnabled);
-        Optional.ofNullable(wireMockProperties.getAsynchronousResponseThreads())
-                .ifPresent(wireMockConfiguration::asynchronousResponseThreads);
-        Optional.ofNullable(wireMockProperties.getRootDirectory())
-                .ifPresent(wireMockConfiguration::usingFilesUnderDirectory);
-
-        if (Boolean.TRUE.equals(wireMockProperties.getJournalDisabled())) {
-            wireMockConfiguration.disableRequestJournal();
+        public WireMockConfigurationFactory(WireMockProperties wireMockProperties) {
+                this.wireMockProperties = wireMockProperties;
         }
 
-        Optional.ofNullable(wireMockProperties.getMaxRequestJournalEntries())
-                .ifPresent(wireMockConfiguration::maxRequestJournalEntries);
-        Optional.ofNullable(wireMockProperties.getGzipDisabled())
-                .ifPresent(wireMockConfiguration::gzipDisabled);
-        Optional.ofNullable(wireMockProperties.getDisableOptimizeXmlFactories())
-                .ifPresent(wireMockConfiguration::disableOptimizeXmlFactoriesLoading);
-        Optional.ofNullable(wireMockProperties.getStubCorsEnabled())
-                .ifPresent(wireMockConfiguration::stubCorsEnabled);
-        Optional.ofNullable(wireMockProperties.getStubRequestLoggingDisabled())
-                .ifPresent(wireMockConfiguration::stubRequestLoggingDisabled);
-        Optional.ofNullable(wireMockProperties.isTemplatingEnabled())
-                .ifPresent(wireMockConfiguration::templatingEnabled);
-        Optional.ofNullable(wireMockProperties.getMappingsClassPath())
-                .ifPresent(wireMockConfiguration::usingFilesUnderClasspath);
-        Optional.ofNullable(wireMockProperties.isProxyPassThrough())
-                .ifPresent(wireMockConfiguration::proxyPassThrough);
-        Optional.ofNullable(wireMockProperties.getJournalDisabled())
-                .filter(Boolean.FALSE::equals)
-                .map(prop -> wireMockConfiguration.requestJournalDisabled());
+        @Bean
+        public WireMockConfiguration wireMockConfiguration() {
+                WireMockConfiguration wireMockConfiguration = new WireMockConfiguration();
 
-        wireMockConfiguration.trustAllProxyTargets(true);
+                Optional.ofNullable(wireMockProperties.getContainerThreads())
+                                .ifPresent(wireMockConfiguration::containerThreads);
+                Optional.ofNullable(wireMockProperties.getAsynchronousResponseEnabled())
+                                .ifPresent(wireMockConfiguration::asynchronousResponseEnabled);
+                Optional.ofNullable(wireMockProperties.getAsynchronousResponseThreads())
+                                .ifPresent(wireMockConfiguration::asynchronousResponseThreads);
+                Optional.ofNullable(wireMockProperties.getRootDirectory())
+                                .ifPresent(wireMockConfiguration::usingFilesUnderDirectory);
 
-        return wireMockConfiguration;
-    }
+                if (Boolean.TRUE.equals(wireMockProperties.getJournalDisabled())) {
+                        wireMockConfiguration.disableRequestJournal();
+                }
+
+                Optional.ofNullable(wireMockProperties.getMaxRequestJournalEntries())
+                                .ifPresent(wireMockConfiguration::maxRequestJournalEntries);
+                Optional.ofNullable(wireMockProperties.getGzipDisabled())
+                                .ifPresent(wireMockConfiguration::gzipDisabled);
+                Optional.ofNullable(wireMockProperties.getDisableOptimizeXmlFactories())
+                                .ifPresent(wireMockConfiguration::disableOptimizeXmlFactoriesLoading);
+                Optional.ofNullable(wireMockProperties.getStubCorsEnabled())
+                                .ifPresent(wireMockConfiguration::stubCorsEnabled);
+                Optional.ofNullable(wireMockProperties.getStubRequestLoggingDisabled())
+                                .ifPresent(wireMockConfiguration::stubRequestLoggingDisabled);
+                Optional.ofNullable(wireMockProperties.isTemplatingEnabled())
+                                .ifPresent(wireMockConfiguration::templatingEnabled);
+                Optional.ofNullable(wireMockProperties.getMappingsClassPath())
+                                .ifPresent(wireMockConfiguration::usingFilesUnderClasspath);
+                Optional.ofNullable(wireMockProperties.isProxyPassThrough())
+                                .ifPresent(wireMockConfiguration::proxyPassThrough);
+                Optional.ofNullable(wireMockProperties.getJournalDisabled())
+                                .filter(Boolean.FALSE::equals)
+                                .map(prop -> wireMockConfiguration.requestJournalDisabled());
+
+                wireMockConfiguration.trustAllProxyTargets(true);
+                wireMockConfiguration.templatingEnabled(true);
+
+                return wireMockConfiguration;
+        }
 }
